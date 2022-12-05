ca715554ab96aba5c81a8cd151f65ad447573dc5
Yeon Woo <evan2011@naver.com>
2022-12-05 17:37:58 +0900

    side menu button making

--- app/src/main/java/com/example/teamproject/SideMenu.java
+++ app/src/main/java/com/example/teamproject/SideMenu.java
@@ -15,9 +15,64 @@ public class SideMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_side_menu);
 
+        // 공지사항 버튼 클릭시 액티비티 전환
+        Button notice_btn = (Button) findViewById(R.id.menu_notice);
+        notice_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StationSearch.class);
+                startActivity(intent);
+            }
+        });
+
         // 역 검색 버튼 클릭시 액티비티 전환
         Button name_rule_btn = (Button) findViewById(R.id.menu_search);
         name_rule_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StationSearch.class);
+                startActivity(intent);
+            }
+        });
+
+        // 분실물 버튼 클릭시 액티비티 전환
+        Button lost_btn = (Button) findViewById(R.id.menu_lost);
+        lost_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StationSearch.class);
+                startActivity(intent);
+            }
+        });
+
+        // 불편신고 버튼 클릭시 액티비티 전환
+        Button uncomfortable_btn = (Button) findViewById(R.id.menu_uncomfortable);
+        uncomfortable_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StationSearch.class);
+                startActivity(intent);
+            }
+        });
+
+        // 즐겨찾기 버튼 클릭시 액티비티 전환
+        Button favorites_btn = (Button) findViewById(R.id.menu_favorites);
+        favorites_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StationSearch.class);
+                startActivity(intent);
+            }
+        });
+
+        // 설정 버튼 클릭시 액티비티 전환
+        Button settings_btn = (Button) findViewById(R.id.menu_settings);
+        settings_btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
